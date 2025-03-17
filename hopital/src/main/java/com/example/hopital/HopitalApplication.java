2681c7e1a0af993074f0fc8a651590cb10089175
nissrine-elf <elfijaoui.nissrine@gamil.com>
2025-03-17 13:46:42 +0000

    TP3

---
+++ hopital/src/main/java/com/example/hopital/HopitalApplication.java
@@ -0,0 +1,28 @@
+package com.example.hopital;
+
+import com.example.hopital.entities.Patient;
+import com.example.hopital.repository.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Date;
+
+@SpringBootApplication
+public class HopitalApplication  implements CommandLineRunner {
+	@Autowired
+	private PatientRepository patientRepository;
+
+	public static void main(String[] args) {
+		SpringApplication.run(HopitalApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		patientRepository.save(new Patient(null,"ali",new Date(),false,22));
+		patientRepository.save(new Patient(null,"amine",new Date(),false,30));
+		patientRepository.save(new Patient(null,"najat",new Date(),true,10));
+
+	}
+}
---
+++ hopital/src/main/java/com/example/hopital/entities/Patient.java
@@ -0,0 +1,20 @@
+package com.example.hopital.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nom;
+    private Date dateNaissance;
+    private boolean malade;
+    private int score;
+}
