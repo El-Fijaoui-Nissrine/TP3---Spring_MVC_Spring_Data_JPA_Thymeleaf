f5f9e098f496a6d5fe9486857d2687a3ec54af2f
nissrine-elf <elfijaoui.nissrine@gamil.com>
2025-03-24 16:02:22 +0000

    TP3_pt3

--- hopital/src/main/java/com/example/hopital/HopitalApplication.java
+++ hopital/src/main/java/com/example/hopital/HopitalApplication.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Date;
 
@@ -24,5 +27,9 @@ public class HopitalApplication  implements CommandLineRunner {
 		//patientRepository.save(new Patient(null,"amine",new Date(),false,30));
         //patientRepository.save(new Patient(null,"najat",new Date(),true,10));
 
+	}
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 }
